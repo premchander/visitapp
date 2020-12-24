fbcac9aa623e68e7b1fd7b774980459a8e404845
premchander.p <prem.chander57@gmail.com>
2020-12-24 14:46:03 +0530

    sample working code

---
+++ index.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const redis = require("redis");
+
+const app = express();
+
+const client = redis.createClient({
+    host: 'redis-server',
+    port: 6379
+});
+
+const port = 8081;
+const host = '0.0.0.0';
+
+client.set('visits', 0);
+
+app.get('/', (req, res) => {
+    client.get('visits', (err, visits) => {
+        res.send("Number of visits" + visits);
+        client.set('visits', parseInt(visits) + 1);
+    })
+});
+
+app.get('/exit', (req, res) => {
+    process.exit(0);
+})
+
+app.listen(port, host);
